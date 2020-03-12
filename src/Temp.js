6aa3e0241b480f503e67a42bf519cb7b69108260
paridhikhaitan <paridhi4699@gmail.com>
2020-03-11 17:36:26 -0700

    new community gif

--- src/Temp.js
+++ src/Temp.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import { Row, Col } from 'react-grid-system';
-import './scss/App.scss'
+import React from "react";
+import { Row, Col, Container } from "react-grid-system";
+import "./scss/App.scss";
 
-class Temp extends React.Component{
-    render(){
-        return(
-            <Row className="temp" justify="center">
-                <Col sm={12} xs={20}>
-                    <img src={require("./images/dcoload.gif")} className="tempImg" draggable="false"/>
-                </Col>
-            </Row>
-        );
-    }
+class Temp extends React.Component {
+  render() {
+    return (
+        <Row className="temp" justify="center" align="center">
+          <Col sm={12} xs={20}>
+            <img
+              src={require("./images/dcoloaddesktop.gif")}
+              className="tempImg"
+              draggable="false"
+            />
+          </Col>
+        </Row>
+    );
+  }
 }
 
 export default Temp;

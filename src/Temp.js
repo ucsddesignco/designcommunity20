32e11b304ccaaa3375f6acc0462bfa5d33c46739
paridhikhaitan <paridhi4699@gmail.com>
2020-03-17 15:56:41 -0700

    yay did the absolute positioning for the main page

--- src/Temp.js
+++ src/Temp.js
@@ -1,17 +1,12 @@
 import React from "react";
-import { Row, Col, Container } from "react-grid-system";
-import "./scss/App.scss";
+import { Col, Row } from "react-grid-system";
 
 class Temp extends React.Component {
   render() {
     return (
-        <Row className="temp" justify="center" align="center">
-          <Col sm={16} xs={20}>
-            <div id="tempImg">
-                <img src={require("./images/dcoloaddesktop.gif")} />
-            </div>
-          </Col>
-        </Row>
+      <div className="temp">
+        <img src={require("./images/dcoloaddesktop.gif")} />
+      </div>
     );
   }
 }

59f75bbb86a34d357da3abd3dccc38949ef18b74
paridhikhaitan <paridhi4699@gmail.com>
2020-03-17 16:25:20 -0700

    woo hoo

--- src/Temp.js
+++ src/Temp.js
@@ -5,11 +5,17 @@ class Temp extends React.Component {
   render() {
     return (
       <div className="temp">
-        <img src={require("./images/dcoloaddesktop.gif")} draggable="false" className="mainImg"/>
+        <img
+          src={require("./images/dcoloaddesktop.gif")}
+          draggable="false"
+          className="mainImg"
+        />
         <h4>Keep an eye out for something we're cooking up...</h4>
         <span>
-          <h3>Main Site</h3>
-          <img src={require("./images/Union.svg")} />
+          <a href="http://www.ucsddesign.co/">
+            <h3>Main Site</h3>
+            <img src={require("./images/Union.svg")} />
+          </a>
         </span>
       </div>
     );

01fc50893b6d451a285b597c0ebfef1593dfdba3
andrewzhen <ajzhen@ucsd.edu>
2020-04-01 14:49:02 -0700

    cut and paste build folder from client to server

--- server/build/service-worker.js
+++ server/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.baa396ffddc23797caa0d1b559de15a5.js"
+  "/precache-manifest.ef433f4cf20b8049f6a33f1509a6a5c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

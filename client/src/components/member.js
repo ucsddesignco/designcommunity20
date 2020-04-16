780004aa99f3e027d5263b838185fde96742a48b
paridhikhaitan <paridhi4699@gmail.com>
2020-04-16 15:57:49 +0530

    tried to enable lazy loading

--- client/src/components/member.js
+++ client/src/components/member.js
@@ -3,17 +3,16 @@ import "../scss/App.scss";
 import { prototype } from "events";
 
 class Member extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      countPeople : 0
-    }
+      countPeople: 0
+    };
 
-    this.handlePeopleLoad = this.handlePeopleLoad.bind(this)
+    this.handlePeopleLoad = this.handlePeopleLoad.bind(this);
   }
 
-  handlePeopleLoad(){
-  }
+  handlePeopleLoad() {}
 
   render() {
     const linkedinURL = `https://www.linkedin.com${this.props.linkedin}`;
@@ -23,14 +22,17 @@ class Member extends React.Component {
     let linkedin = this.props.linkedin.toUpperCase();
     return (
       <div className="memberContainer">
-        <img src={require(`../images/team/${this.props.image}.jpeg`)} onLoad={this.handlePeopleLoad}></img>
+        <img
+          src={require(`../images/team/${this.props.image}.jpeg`)}
+          loading="lazy"
+        ></img>
         <p>CLASS OF {this.props.year} </p>
         <h2> {this.props.name} </h2>
         <div className="info">
           <img src={require("../images/globeVector.svg")}></img>
           <p>
             {" "}
-            <a href= {portfolioURL}>{portfolio}</a>
+            <a href={portfolioURL}>{portfolio}</a>
           </p>
         </div>
         <div className="info">
